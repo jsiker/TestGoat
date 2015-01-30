927c028905a37dec48e1b7e635d40b5f03706f42
Jon Siker <jon.siker@gmail.com>
2015-01-29 22:05:59 -0800

    first commit

---
+++ functional_tests.py
@@ -0,0 +1,36 @@
+__author__ = 'danielsiker'
+
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def user_can_start_a_list_and_retrieve_it_later(self):
+        # lily wants to check out our cool new to-do list app
+        # let's check it out, no?
+        self.browser.get('http://localhost:8000')
+
+        # she notices the silly title
+        self.assertIn('Go At It', self.browser.title)
+
+        # she's invited to enter a 'goat' item right away
+
+        # she types "brew monkey tea" into a text box
+
+        # when she hits enter, the page updates
+
+        # there is still a text box (now empty) inviting her to add another item
+        # she enters "drink monkey tea, slowly"
+
+        # the page updates again, now she has 2 items on her list
+
+        # lily sees our unique URL for her
+
+        # she goes to that URL. and rejoices.
