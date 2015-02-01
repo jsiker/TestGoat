a4c765260f0d270125f1cc0722cc6b18060f930f
Jon Siker <jon.siker@gmail.com>
2015-01-31 19:51:23 -0800

    helper function to check table rows

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table_list(self, row_text):
+        table = self.browser.find_element_by_id('list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # lily wants to check out our cool new to-do list app
         # let's check it out, no?
@@ -37,10 +42,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # when she hits enter, the page updates
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('list_table')
-        rows = table.find_elements_by_tag_name('tr')  # find_elementS returns a list which may be empty
-        self.assertIn('1. brew monkey tea', [row.text for row in rows])
+        self.check_for_row_in_table_list('1: brew monkey tea')
 
         # there is still a text box (now empty) inviting her to add another item
         # she enters "drink monkey tea, slowly"
@@ -48,15 +50,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('drink monkey tea, slowly')
         inputbox.send_keys(Keys.ENTER)
 
-
         # the page updates again, now she has 2 items on her list
-        table = self.browser.find_element_by_id('list_table')
-        rows = table.find_elements_by_tag_name('tr')  # find_elementS returns a list which may be empty
-        self.assertIn('1. brew monkey tea', [row.text for row in rows])
-        self.assertIn(
-            '2: drink monkey tea, slowly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_table_list('1: brew monkey tea')
+        self.check_for_row_in_table_list('2: drink monkey tea, slowly')
 
         # lily sees our unique URL for her
 
